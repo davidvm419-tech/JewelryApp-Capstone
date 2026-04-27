7783e054263da238e5deef42e7e330b0a0296466
Juan David <davidvm419@gmail.com>
2026-04-27 17:45:41 -0500

    fixed 404 react error on reload.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.views.static import serve
+from django.urls import include, path, re_path  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("store.urls")),
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 
